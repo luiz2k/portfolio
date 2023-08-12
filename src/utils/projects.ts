4436ccc95e011060e8d8c1174b8186171bcc0cf9
Luiz Teles <luizteles934@gmail.com>
2023-08-12 15:44:50 -0300

    Alteração na descrição do projeto Carrinho de Compras

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -9,7 +9,7 @@ const projects = [
     title: 'Carrinho de Compras',
     image: carrinhoDeCompras,
     description:
-      'Projeto de um Carrinho de Compras feito no NextJS com TypeScript, consumindo a API do Mercado Livre e utilizando o React Query para o consumo da API. A página inicial exibe todos os produtos disponíveis para adicionar ao carrinho. Após a adição de um produto, um contador aparece no canto superior direito, mostrando a quantidade de itens no carrinho. Na página do carrinho, é apresentado um resumo dos produtos adicionados, incluindo a quantidade e o preço total. Para compartilhar os dados entre a página inicial e a do carrinho, a Context API foi utilizada. O layout da aplicação foi construído com o auxílio do Sass.',
+      'Projeto de um Carrinho de Compras feito no NextJS com TypeScript, consumindo a API do Mercado Livre e utilizando o React Query para o consumo da API. A página inicial exibe todos os produtos disponíveis para adicionar ao carrinho. Após a adição de um produto, um contador aparece no canto superior direito, mostrando a quantidade de itens no carrinho. Na página do carrinho, é apresentado um resumo dos produtos adicionados, incluindo a quantidade e o preço total. Para compartilhar os dados entre a página inicial e a do carrinho, a Context API foi utilizada. O layout da aplicação foi construído com o Sass e CSS Modules.',
     technologies: [
       'HTML',
       'CSS',
