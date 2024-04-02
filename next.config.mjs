c34c56460df08ea107e562b181cd25b55189e768
Luiz Teles <luizteles934@gmail.com>
2024-04-02 15:15:39 -0300

    feat: Autorizar a utilização de imagens do github e imgur

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "/**.png",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
