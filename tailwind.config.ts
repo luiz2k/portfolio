380d979a2b7244e716a865644d58a8418ff97933
Luiz Teles <luizteles934@gmail.com>
2024-04-06 16:11:47 -0300

    feat: Peleta de cores de aplicação

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "color-1": "#f8fafc",
+        "color-2": "#f1f5f9",
+        "color-3": "#6b7280",
+        "color-4": "#1f2937",
+        "color-5": "#111827",
       },
     },
   },
