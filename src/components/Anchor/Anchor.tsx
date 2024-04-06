ab3645dd361369b397831e56fce07ceb3de6d508
Luiz Teles <luizteles934@gmail.com>
2024-04-06 16:11:29 -0300

    feat: Implementar uma componente de âncora customizavel

---
+++ src/components/Anchor/Anchor.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+interface AnchorProps extends ComponentProps<"a"> {
+  href: string;
+  pathName?: string;
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
+  children: string;
+}
+
+export default function Anchor({
+  href,
+  pathName,
+  leftIcon,
+  rightIcon,
+  children,
+  ...props
+}: AnchorProps) {
+  return (
+    <Link
+      href={href}
+      data-path={pathName ? pathName === href : null}
+      className="flex items-center justify-between rounded p-1.5 duration-150 hover:bg-color-3/5 data-[path=true]:cursor-default data-[path=true]:bg-color-3/10"
+      {...props}
+    >
+      <span className="flex items-center gap-1">
+        {leftIcon} {children}
+      </span>
+      <span>{rightIcon}</span>
+    </Link>
+  );
+}
