06e2ecaa8419e700e0cc11c2704d70c4a8b77738
Luiz Teles <luizteles934@gmail.com>
2024-04-13 15:43:31 -0300

    feat: Implementar um distintivo

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,19 @@
+import { skillIcons } from "@/utils/icons";
+
+interface BadgeProps {
+  title?: string;
+  icon?: string;
+  description?: string;
+  children?: string;
+}
+
+export default function Badge({ title, icon, children }: BadgeProps) {
+  return (
+    <article
+      title={title}
+      className="inline-flex items-center gap-1 rounded bg-color-3/15 p-2 duration-150 ease-in-out hover:bg-color-3/10"
+    >
+      <span className="text-2xl">{icon && skillIcons(icon)}</span> {children}
+    </article>
+  );
+}
