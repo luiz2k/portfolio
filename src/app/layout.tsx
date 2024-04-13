a818e898630552d9cb550bd00fefa810205805c7
Luiz Teles <luizteles934@gmail.com>
2024-04-13 16:06:39 -0300

    feat: Implementar o provedor de temas na aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Poppins } from "next/font/google";
 
 import Header from "@/components/Header/Header";
 
+import ThemeProvider from "@/providers/ThemeProvider";
+
 import "@/styles/globals.css";
 
 const poppins = Poppins({
@@ -23,12 +25,14 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={poppins.className}>
-        <div className="bg-color-1 bg-[url('/noise.webp')] text-black dark:bg-color-5 dark:text-white">
-          <Header />
-          <main className="mt-[4.688rem] px-5 py-16 md:ml-60 md:mt-0">
-            <div className="m-auto max-w-4xl">{children}</div>
-          </main>
-        </div>
+        <ThemeProvider>
+          <div className="bg-color-1 bg-[url('/noise.webp')] text-black dark:bg-color-5 dark:text-white">
+            <Header />
+            <main className="mt-[4.688rem] px-5 py-16 md:ml-60 md:mt-0">
+              <div className="m-auto max-w-4xl">{children}</div>
+            </main>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
