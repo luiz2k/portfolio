fdc5648274769dabadf68d8649fc7ca246058bf6
Luiz Teles <luizteles934@gmail.com>
2024-04-13 16:03:34 -0300

    feat: Implementar o cabeçalho da aplicação e definir as medias do conteúdo principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import Header from "@/components/Header/Header";
+
 import "@/styles/globals.css";
 
 const poppins = Poppins({
@@ -21,7 +23,12 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={poppins.className}>
-        <main>{children}</main>
+        <div className="bg-color-1 bg-[url('/noise.webp')] text-black dark:bg-color-5 dark:text-white">
+          <Header />
+          <main className="mt-[4.688rem] px-5 py-16 md:ml-60 md:mt-0">
+            <div className="m-auto max-w-4xl">{children}</div>
+          </main>
+        </div>
       </body>
     </html>
   );
