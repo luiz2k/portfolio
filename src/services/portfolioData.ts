b51698c3cc940b81199c8ffb7f72c73aebd3258f
Luiz Teles <luizteles934@gmail.com>
2024-04-18 02:20:56 -0300

    feat: Revalidar o cache da requisição HTTP a cada uma hora

--- src/services/portfolioData.ts
+++ src/services/portfolioData.ts
@@ -4,7 +4,9 @@ const URL: string =
   "https://gist.githubusercontent.com/luiz2k/94036aaaa0dad496b37f9099aa2e7e86/raw";
 
 export default async function getPortfolioData(): Promise<Data> {
-  const response = await fetch(URL);
+  const ONE_HOUR: number = 3600;
+
+  const response = await fetch(URL, { next: { revalidate: ONE_HOUR } });
   const data: Data = await response.json();
 
   return data;
