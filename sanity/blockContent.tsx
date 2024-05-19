1758a42dc0b12a39ceea7d5098545df69bd47a68
Luiz Teles <luizteles934@gmail.com>
2024-05-19 19:45:16 -0300

    feat: Schema de um bloco de escrita para o Sanity

---
+++ sanity/blockContent.tsx
@@ -0,0 +1,44 @@
+import { defineArrayMember, defineType } from "sanity";
+
+export default defineType({
+  title: "Block Content",
+  name: "blockContent",
+  type: "array",
+  of: [
+    defineArrayMember({
+      title: "Block",
+      type: "block",
+      marks: {
+        decorators: [{ title: "Strong", value: "strong" }],
+        annotations: [
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+              {
+                title: "Target",
+                name: "target",
+                type: "string",
+                options: {
+                  list: [
+                    { title: "Self", value: "_self" },
+                    { title: "Blank", value: "_blank" },
+                  ],
+                },
+                initialValue: "_self",
+              },
+            ],
+          },
+        ],
+      },
+      lists: [],
+      styles: [],
+    }),
+  ],
+});
