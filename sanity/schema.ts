e86cb82a796503e06ece13d23a3aa084346248d1
Luiz Teles <luizteles934@gmail.com>
2024-05-19 20:38:49 -0300

    feat: Importar os schemas do sanity

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,11 @@
 import { type SchemaTypeDefinition } from "sanity";
+import { me } from "./me";
+import { projects } from "./projects";
+import { skills } from "./skills";
+import { skillCategories } from "./skillCategories";
+import blockContent from "./blockContent";
+import { contacts } from "./contacts";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [me, projects, skills, skillCategories, blockContent, contacts],
 };
