e556934b2edd81bcbc17318c001eb9b202c220ae
Luiz Teles <luizteles934@gmail.com>
2024-05-19 23:51:59 -0300

    feat: Receber os dados do Sanity na página de contatos

--- src/app/(public)/contatos/page.tsx
+++ src/app/(public)/contatos/page.tsx
@@ -1,11 +1,10 @@
+import { getContacts } from "@/api/sanityServices";
 import ContactCard from "@/components/ContactCard/ContactCard";
-
-import getPortfolioData from "@/services/portfolioData";
 
 import Link from "next/link";
 
 export default async function Contacts() {
-  const data = await getPortfolioData();
+  const contacts = await getContacts();
 
   return (
     <section className="space-y-24">
@@ -19,21 +18,21 @@ export default async function Contacts() {
         </h2>
 
         <div className="ml-0 grid grid-cols-[repeat(auto-fill,15rem)] justify-center gap-5 md:ml-5 lg:justify-normal">
-          {data.contacts.map((contact) =>
-            contact.link ? (
-              <Link key={contact.title} href={contact.link} target="_blank">
+          {contacts.map((contact) =>
+            contact.has_url ? (
+              <Link key={contact.title} href={contact.url} target="_blank">
                 <ContactCard
                   title={contact.title}
-                  icon={contact.title}
                   description={contact.description}
+                  imageUrl={contact.imageUrl}
                 />
               </Link>
             ) : (
               <ContactCard
                 key={contact.title}
                 title={contact.title}
-                icon={contact.title}
                 description={contact.description}
+                imageUrl={contact.imageUrl}
               />
             ),
           )}
