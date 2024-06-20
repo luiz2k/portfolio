356f9bf85a03f1926f03bbf429ff95ef06d527d9
luiz2k <luizteles934@gmail.com>
2024-06-20 14:06:19 -0300

    Definir uma tamanho para a imagem da badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -8,7 +8,14 @@ export default function Badge({ title, imageUrl, children }: BadgeProps) {
       className="group relative inline-flex items-center gap-1 rounded bg-color-3/15 p-2 duration-150 ease-in-out hover:bg-color-3/10"
     >
       {imageUrl && (
-        <Image key={title} src={imageUrl} alt={title} width={20} height={20} />
+        <Image
+          key={title}
+          src={imageUrl}
+          alt={title}
+          width={20}
+          height={20}
+          className="size-5"
+        />
       )}
 
       {!children && (
