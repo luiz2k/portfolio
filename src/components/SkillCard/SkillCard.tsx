98e8814b7e0fe976bc99ddf162c2c3b885a06715
luiz2k <luizteles934@gmail.com>
2024-06-20 14:06:03 -0300

    Definir uma tamanho para o imagem da habilidade

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -8,7 +8,13 @@ export default function SkillCard({ title, imageUrl }: SkillCardProps) {
       title={title}
       className="flex size-32 flex-col items-center justify-center gap-1 rounded bg-color-3/15 p-2 text-center duration-150 ease-in-out hover:bg-color-3/10"
     >
-      <Image src={imageUrl} alt={title} width={70} height={70} />
+      <Image
+        src={imageUrl}
+        alt={title}
+        width={70}
+        height={70}
+        className="size-[4.375rem]"
+      />
       <span>{title}</span>
     </article>
   );
