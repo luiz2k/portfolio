1fd0bd75d3d8b3fbf214d973179ef402d6c026ba
luiz2k <luizteles934@gmail.com>
2024-08-02 18:39:52 -0300

    Importar o novo schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,20 @@
-import { type SchemaTypeDefinition } from "sanity";
+import type { SchemaTypeDefinition } from "sanity";
 import { me } from "./me";
 import { projects } from "./projects";
 import { skills } from "./skills";
 import { skillCategories } from "./skillCategories";
-import blockContent from "./blockContent";
 import { contacts } from "./contacts";
+import { projectCategories } from "./projectCategories";
+import blockContent from "./blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [me, projects, skills, skillCategories, blockContent, contacts],
+  types: [
+    me,
+    projects,
+    projectCategories,
+    skills,
+    skillCategories,
+    blockContent,
+    contacts,
+  ],
 };
