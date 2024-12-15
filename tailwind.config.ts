96cff6c27b0c1e0fca5c393d0f846808c6e3b9c7
luiz2k <luizteles934@gmail.com>
2024-12-15 17:54:13 -0300

    Preparar ambiente de desenvolvimento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,19 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: "#1E1E1E",
+        white: "#F3F4F6",
+        accent: "#C39B4B",
+      },
+      blur: {
+        xs: "2px",
       },
     },
   },
