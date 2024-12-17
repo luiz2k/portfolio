7b71d1f98042d5d1206b5cfe788ac0fcc3359a4c
luiz2k <luizteles934@gmail.com>
2024-12-17 01:46:51 -0300

    Estilizar componente layout - Adicionar header - Adicionar footer - Adicionar o grid como plano de fundo - Preparar o local onde vai receber os childrens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from "@/modules/shared/components/Footer/Footer";
+import { Header } from "@/modules/shared/components/Header/Header";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 
@@ -12,8 +14,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body>{children}</body>
+    <html
+      lang="pt-BR"
+      className="bg-white after:fixed after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:bg-[url('/grid.webp')] after:opacity-[5%] after:content-['']"
+    >
+      <body>
+        <Header />
+
+        <div className="mx-3.5 min-h-screen pb-10 pt-[7.688rem]">
+          <main className="m-auto max-w-3xl">{children}</main>
+        </div>
+
+        <Footer />
+      </body>
     </html>
   );
 }
