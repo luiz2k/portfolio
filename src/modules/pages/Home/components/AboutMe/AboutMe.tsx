86d80e3b9178b4288ab4486646a46054ef3569e8
luiz2k <luizteles934@gmail.com>
2024-12-17 16:35:57 -0300

    Obter o texto sobre mim da função de informações principais

--- src/modules/pages/Home/components/AboutMe/AboutMe.tsx
+++ src/modules/pages/Home/components/AboutMe/AboutMe.tsx
@@ -1,14 +1,13 @@
+import { getMainInformations } from "@/modules/shared/functions/getInformations";
+
 export function AboutMe() {
+  const mainInformations = getMainInformations();
+
   return (
     <section className="flex flex-col gap-2">
       <h2 className="m-auto text-xl font-bold sm:m-0">Sobre mim</h2>
 
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not.
-      </p>
+      <p>{mainInformations.aboutMe}</p>
     </section>
   );
 }
