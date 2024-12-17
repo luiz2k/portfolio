ec133cca1ce08a66f65f8f4d25f037b468d657ea
luiz2k <luizteles934@gmail.com>
2024-12-17 00:28:24 -0300

    Criar função para obter os ícones através do seu nome

---
+++ src/modules/shared/functions/getIcons.ts
@@ -0,0 +1,26 @@
+// Obtém os icones através do nome da linguagem
+export const getIcons = (icon: string) => {
+  return {
+    TypeScript: "/icons/typescript.svg",
+    "React.JS": "/icons/react-js.svg",
+    "Next.JS": "/icons/next-js.svg",
+    "Tailwind CSS": "/icons/tailwind-css.svg",
+    Astro: "/icons/astro.svg",
+    "Express.JS": "/icons/express-js.svg",
+    Vitest: "/icons/vitest.svg",
+    Cypress: "/icons/cypress.svg",
+    "Node.JS": "/icons/node-js.svg",
+    PHP: "/icons/php.svg",
+    PostgreSQL: "/icons/postgresql.svg",
+    HTML: "/icons/html.svg",
+    CSS: "/icons/css.svg",
+    JavaScript: "/icons/javascript.svg",
+    TypeORM: "/icons/typeorm.svg",
+    Prisma: "/icons/prisma.svg",
+    "Nest.JS": "/icons/nest-js.svg",
+    MongoDB: "/icons/mongodb.svg",
+    Docker: "/icons/docker.svg",
+    Zustand: "/icons/zustand.svg",
+    Swagger: "/icons/swagger.svg",
+  }[icon];
+};
