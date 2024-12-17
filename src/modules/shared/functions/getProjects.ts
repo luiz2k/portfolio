395b0e66aef6f557b642df2b520f20a5d4089dd2
luiz2k <luizteles934@gmail.com>
2024-12-17 01:01:19 -0300

    Criar função para obter todos os projetos

---
+++ src/modules/shared/functions/getProjects.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import matter from "gray-matter";
+import { getAboutMe } from "./getAboutMe";
+
+type GetProjects = {
+  content: string;
+  data: {
+    slug: string;
+    title: string;
+    image: string;
+    technologies: string[];
+    links: {
+      sourceCode: string;
+      demo: string;
+      docs: string;
+      figma: string;
+    };
+  };
+}[];
+
+// Obtém todos os serviços
+export const getAllProjects = (): GetProjects => {
+  const folder = "portfolio/projects";
+
+  const fileNames = fs.readdirSync(folder);
+
+  // Obtém o conteúdo dos arquivos
+  const services = fileNames.map((fileName) => {
+    const archive = folder + "/" + fileName;
+
+    return fs.readFileSync(archive, "utf-8");
+  });
+
+  const matters = services.map((project) => matter(project));
+
+  return matters as unknown as GetProjects;
+};
+
+// Obtém os serviços principais
+export const getMainProjects = (): GetProjects => {
+  const folder = "portfolio/projects";
+
+  const fileNames = fs.readdirSync(folder);
+
+  const mainServicesSlug = getAboutMe().mainProjects;
+
+  // Filtrando os serviços principais
+  const mainServices = fileNames.filter((fileName) => {
+    const slug = fileName.split(".")[0];
+
+    return mainServicesSlug.includes(slug);
+  });
+
+  // Obtém o conteúdo dos arquivos
+  const services = mainServices.map((fileName) => {
+    const archive = folder + "/" + fileName;
+
+    return fs.readFileSync(archive);
+  });
+
+  const matters = services.map((project) => matter(project));
+
+  return matters as unknown as GetProjects;
+};
