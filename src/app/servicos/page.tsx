0c524796a74e52c938f2e2a7093472fc7ccdcedb
luiz2k <luizteles934@gmail.com>
2024-12-17 01:47:59 -0300

    Criar página de serviços

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,3 @@
+import { ServicesPage } from "@/modules/pages/Services/ServicesPage";
+
+export default ServicesPage;
---
+++ src/modules/pages/Services/ServicesPage.tsx
@@ -0,0 +1,29 @@
+import { IntroductoryTitle } from "@/modules/shared/components/IntroductoryTitle/IntroductoryTitle";
+import { ProjectCard } from "@/modules/shared/components/ProjectCard/ProjectCard";
+import { getAllServices } from "@/modules/shared/functions/getServices";
+
+export function ServicesPage() {
+  const services = getAllServices();
+
+  return (
+    <section className="grid gap-10">
+      <IntroductoryTitle
+        title="Serviços"
+        description="Aqui você pode ver meus serviços"
+      />
+
+      <section className="grid gap-5">
+        {services.map((service) => (
+          <ProjectCard
+            key={service.data.slug}
+            title={service.data.title}
+            image={service.data.image}
+            description={service.content}
+            technologies={service.data.technologies}
+            links={service.data.links}
+          />
+        ))}
+      </section>
+    </section>
+  );
+}
