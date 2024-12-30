d7b3a1b64b0439177a1b38b7db881c76e3288bcf
luiz2k <luizteles934@gmail.com>
2024-12-30 17:53:47 -0300

    Adicionar a tag li na tag ul do rodapé

--- src/modules/shared/components/Footer/Footer.tsx
+++ src/modules/shared/components/Footer/Footer.tsx
@@ -10,29 +10,33 @@ export function Footer() {
           <h2 className="font-bold text-white">Sobre o Portfólio</h2>
 
           <ul className="flex flex-col items-center gap-1 sm:items-stretch">
-            <span className="flex gap-1 text-white">
-              <Github size="24" strokeWidth="1.5" />
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/luiz2k/portfolio"
-                className="hover:underline"
-              >
-                Código-fonte
-              </Link>
-            </span>
+            <li>
+              <span className="flex gap-1 text-white">
+                <Github size="24" strokeWidth="1.5" />
+                <Link
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/luiz2k/portfolio"
+                  className="hover:underline"
+                >
+                  Código-fonte
+                </Link>
+              </span>
+            </li>
 
-            <span className="flex gap-1 text-white">
-              <Figma size="24" strokeWidth="1.5" />
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.figma.com/design/uBT2kkwWmCFddB8G0hXqbM/Portf%C3%B3lio?node-id=0-1&t=r4frP4C1ozmZA59S-1"
-                className="hover:underline"
-              >
-                Figma
-              </Link>
-            </span>
+            <li>
+              <span className="flex gap-1 text-white">
+                <Figma size="24" strokeWidth="1.5" />
+                <Link
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.figma.com/design/uBT2kkwWmCFddB8G0hXqbM/Portf%C3%B3lio?node-id=0-1&t=r4frP4C1ozmZA59S-1"
+                  className="hover:underline"
+                >
+                  Figma
+                </Link>
+              </span>
+            </li>
           </ul>
         </div>
 
