4d12b8f636355f0b736bd94796bf5bc5dadda45f
luiz2k <luizteles934@gmail.com>
2025-01-28 21:16:16 -0300

    Implementar fonte Roboto em todo o layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/modules/shared/components/Footer/Footer";
 import { Header } from "@/modules/shared/components/Header/Header";
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
 import "@/styles/globals.css";
 
@@ -8,6 +9,11 @@ export const metadata: Metadata = {
   description:
     "Tenho 24 anos, sou um desenvolvedor full stack com foco em Next.JS e Node.JS, graduado em analise e desenvolvimento de sistemas, atualmente em busca de entrar na carreira de desenvolvimento.",
 };
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -19,7 +25,7 @@ export default function RootLayout({
       lang="pt-BR"
       className="bg-white after:fixed after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:bg-[url('/grid.webp')] after:opacity-[5%] after:content-['']"
     >
-      <body>
+      <body className={roboto.className}>
         <Header />
 
         <div className="mx-3.5 min-h-screen pb-10 pt-[7.688rem]">
