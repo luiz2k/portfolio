d0b3135786e93bab86b04beaedbe8ab32e3d56e7
luiz2k <luizteles934@gmail.com>
2025-01-28 21:50:10 -0300

    Melhorias no componente de âncora
    - Definir tipagem do componente
    - Pré carregar as páginas
    - Usar componente Link do Next

--- src/modules/shared/components/Header/components/NavigationAnchor/NavigationAnchor.tsx
+++ src/modules/shared/components/Header/components/NavigationAnchor/NavigationAnchor.tsx
@@ -1,25 +1,35 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
-type HeaderAnchorProps = {
+type HeaderAnchorProps = React.ComponentProps<typeof Link> & {
   href: string;
   children: React.ReactNode;
 };
 
-export function NavigationAnchor({ href, children }: HeaderAnchorProps) {
+export function NavigationAnchor({
+  href,
+  children,
+  ref,
+  ...rest
+}: HeaderAnchorProps) {
   const pathname = usePathname();
+  const router = useRouter();
 
   // Verifica se a página atual corresponde ao `href`
   const currentPage = pathname === href;
 
   return (
-    <a
+    <Link
       href={href}
       data-currentpage={currentPage}
+      onMouseEnter={() => router.prefetch(href)} // Precarrega a página
+      ref={ref} // Passa o ref para o Link
       className="text-black hover:text-accent data-[currentpage=true]:pointer-events-none data-[currentpage=true]:cursor-text data-[currentpage=true]:font-bold data-[currentpage=true]:text-accent"
+      {...rest}
     >
       {children}
-    </a>
+    </Link>
   );
 }
