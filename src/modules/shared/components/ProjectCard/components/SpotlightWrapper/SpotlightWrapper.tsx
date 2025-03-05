267da63f825bce713db7f0288f76c31802b37a95
luiz2k <luizteles934@gmail.com>
2025-03-05 17:39:09 -0300

    styles: Alterar a cor do efeito hover de fundo

--- src/modules/shared/components/ProjectCard/components/SpotlightWrapper/SpotlightWrapper.tsx
+++ src/modules/shared/components/ProjectCard/components/SpotlightWrapper/SpotlightWrapper.tsx
@@ -5,7 +5,7 @@ import SpotlightCard from "../../../_bits/SpotlightCard/SpotlightCard";
 export function SpotlightWrapper({ children }: { children: React.ReactNode }) {
   return (
     <SpotlightCard
-      spotlightColor="rgba(999, 999, 999, 100)"
+      spotlightColor="rgba(30, 30, 30, 0.15)"
       className="rounded-lg shadow-sm"
     >
       {children}
